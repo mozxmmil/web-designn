03b6120bed5c621fa968f822b2e0122e6e8fc12a
mozxmmi <mdmozammil605@gmail.com>
2024-12-19 22:46:09 +0530

    project 5 Hero section completed

--- src/app/project5/page.tsx
+++ src/app/project5/page.tsx
@@ -1,12 +1,53 @@
-import React from 'react'
+import React from "react";
+import { GoArrowRight } from "react-icons/go";
+import { IoMdSearch } from "react-icons/io";
 
 const page = () => {
   return (
-    <div className='w-full h-screen bg-yellow-50'>
-      <h1>hello ji</h1>
+    <main className="w-full h-screen bg-yellow-50 pt-[0.1px]">
+      <nav className="w-full h-[10vh]  mt-10  border-black border-t-2 border-b-2 flex items-center justify-between px-20">
+        <h1 className=' border-black h-full w-[25%]   flex justify-center items-center font-[Broadway] font-bold text-4xl after:content-["\00A9"] relative after:absolute  after:right-[25%] after:top-1 after:text-[1rem]'>
+          primer
+        </h1>
+        <h2 className="border-r-2 border-black h-full w-[20%]   flex justify-center items-center font-gilroy_M font-bold text-2xl">
+          Model
+        </h2>
+        <h2 className="border-r-2 border-black h-full w-[20%]   flex justify-center items-center font-gilroy_M font-bold text-2xl">
+          Women/Men
+        </h2>
+        <h2 className="border-r-2 border-black h-full w-[29%]   flex justify-center items-center font-gilroy_M font-bold text-2xl">
+          Agency/Blog/Information
+        </h2>
+        <IoMdSearch className="border-black h-1/2 w-[10%] flex justify-center items-center font-thin" />
+      </nav>
+      <section className="w-full h-[84.5vh]  p-14 relative">
+        <div className="innterdiv  h-full w-full relative">
+          <video
+            autoPlay
+            muted
+            loop
+            className="w-full h-full object-cover"
+            src="/Dior.mp4"
+          ></video>
+          <div className="bottomSection absolute bottom-0 w-full h-[40%] px-10 text-white">
+            <div className="first  w-full h-[60%] flex justify-between items-center border-b-2 border-white pb-5 ">
+              <h1 className="font-monument  text-6xl font-bold">
+                Photography <br />
+                and film
+              </h1>
+              <h1 className="px-7 py-5 rounded-full border-2 w-20 h-20 flex items-center justify-center">
+                01
+              </h1>
+            </div>
+            <div className="second  w-full h-[40%] flex justify-between items-center ">
+              <h1 className="font-medium">Explore</h1>
+              <GoArrowRight className="h-10 w-10" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
 
-    </div>
-  )
-}
-
-export default page
+export default page;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,23 @@
 import type { Config } from "tailwindcss";
-import plugin from 'tailwindcss/plugin';
-import type { CSSRuleObject } from 'tailwindcss/types/config';
+import plugin from "tailwindcss/plugin";
+import type { CSSRuleObject } from "tailwindcss/types/config";
 
 const scrollbarPlugin = plugin(function ({ addUtilities }) {
   const scrollbarUtilities: { [key: string]: CSSRuleObject } = {
-    '.scrollbar': {
-      '&::-webkit-scrollbar': {
-        backgroundColor: 'transparent',
-        height: '10px',
-        width: '10px',
+    ".scrollbar": {
+      "&::-webkit-scrollbar": {
+        backgroundColor: "transparent",
+        height: "10px",
+        width: "10px",
       },
-      '&::-webkit-scrollbar-thumb': {
-        backgroundColor: 'red',
+      "&::-webkit-scrollbar-thumb": {
+        backgroundColor: "red",
       },
     },
   };
 
   // Add the custom scrollbar utilities
-  addUtilities(scrollbarUtilities,);
+  addUtilities(scrollbarUtilities);
 });
 
 export default {
@@ -28,18 +28,23 @@ export default {
   ],
   theme: {
     extend: {
-      keyframes:{
-        rotate:{
-          from:{
-            left:"0%"
-          },
-          to :{
-            right:"100%"
-          }
-        }
+      fontFamily: {
+        Gilroy_R :["var(--Gilroy-R)","sans-serif"],
+        gilroy_M: ["var(--Gilroy-M)", "sans-serif"], // Reference the font name from @font-face
+        monument: ["var(--Monument)", "sans-serif"], // Reference the font name from @font-face
       },
-      animation:{
-        nope:'rotate '
+      keyframes: {
+        rotate: {
+          from: {
+            left: "0%",
+          },
+          to: {
+            right: "100%",
+          },
+        },
+      },
+      animation: {
+        nope: "rotate ",
       },
       colors: {
         background: "var(--background)",
