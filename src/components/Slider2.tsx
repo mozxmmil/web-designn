f3f0ae8d8f0f913640471139c0e21632ee3074b6
mozxmmi <mdmozammil605@gmail.com>
2024-12-25 23:47:03 +0530

    slider2 Responsive done

--- src/components/Slider2.tsx
+++ src/components/Slider2.tsx
@@ -7,7 +7,7 @@ interface ImagePropes {
 }
 
 const ImageComp: React.FC<ImagePropes> = memo(({ url }) => (
-  <div className="circle h-32 w-32   rounded-full  relative overflow-hidden  ">
+  <div className="circle md:h-32 md:w-32 h-20 w-20   rounded-full  relative overflow-hidden  ">
     <Image src={url} alt="not" fill className="object-cover" quality={10} />
   </div>
 ));
@@ -16,7 +16,7 @@ ImageComp.displayName = "ImageComponent";
 
 const Slider2 = () => {
   return (
-    <div className="w-full  flex gap-5 overflow-x-hidden overflow-y-hidden  mb-5   relative">
+    <div className="w-full  flex gap-5 overflow-x-hidden overflow-y-hidden  mb-5 mt-5 relative">
       <div className="flex gap-5  animate-slider items-center justify-center ">
         {[...ImageSlider, ...ImageSlider].map((value, index) => (
           <ImageComp key={index} url={value.url} />
